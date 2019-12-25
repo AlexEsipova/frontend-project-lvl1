a0fb703df954dab18494f1d71a02b25b25b4093f
Sasha Esipova <89099438452@mail.ru>
2019-12-26 00:05:38 +0300

    failed third step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
 console.log('Welcome to the Brain Games!');
+
+import userName from '..';
+
+console.log('Hello, ' + userName + '!');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('May I have your name? ');
+
+export default userName;
